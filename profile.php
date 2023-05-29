029bb0f75015c13a133c3526db26e3498d054828
KamadoSama <tahoarthur52@gmail.com>
2023-05-29 10:48:04 +0000

    #refactor

--- profile.php
+++ profile.php
@@ -19,8 +19,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/profil.css">
-    <link rel="stylesheet" href="./bootstrap-5.3.0-alpha3-dist/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
-    <script src="./bootstrap-5.3.0-alpha3-dist/bootstrap-5.3.0-alpha3-dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/2dcb2c25bd.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Profil</title>
 <?php include("./header.php") ?>
     <div id="deco_mini">
@@ -46,15 +47,100 @@
             <section>
                 <div id="container_username">
                     <h3>Nom d'utilisateur: Kamado12</h3>
-                    <button>Modifier le mot de passe</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"  data-bs-target="#exampleModal">Modifier le mot de passe</button>
                 </div>
             </section>
-            <div>
-                
-            </div>
+            <section class="matiere">
+                <h2>Matières</h2>
+                <table id="coursTable">
+                    <thead>
+                    <tr>
+                        <th>UE</th>
+                        <th>Classe</th>   
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p>python</p>
+                            </td>
+                            <td>
+                                <p>M1SGIL</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>python</p>
+                            </td>
+                            <td>
+                                <p>M1SGIL</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
         </section>
     </main>
     <div id="deco_big">
 
     </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modifier le mot de passe <span></span> </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="/evaluation/sociabilite" id="form-evaluation" method="post">
+                <div class="form-group">
+                    <label>Mot de passe actuel: </label>
+                    <div class="input-group" id="show_hide_password">
+                    <input class="form-control" type="password">
+                    <div class="input-group-text">
+                        <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                    </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Nouveau mot de passe: </label>
+                    <div class="input-group" id="show_hide_password">
+                    <input class="form-control" type="password">
+                    <div class="input-group-text">
+                        <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                    </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Modifier</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+            </div>
+            </div>
+        </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+        <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if($('#show_hide_password input').attr("type") == "text"){
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass( "fa-eye-slash" );
+                    $('#show_hide_password i').removeClass( "fa-eye" );
+                }else if($('#show_hide_password input').attr("type") == "password"){
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass( "fa-eye-slash" );
+                    $('#show_hide_password i').addClass( "fa-eye" );
+                }
+            });
+        });
+    </script>
 <?php include("./footer.php") ?>
